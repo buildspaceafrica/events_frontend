ffef2f278e999070c9373e94a89188676b5cc239
davien21 <chidiebereekennia@gmail.com>
2022-01-19 08:14:56 +0100

    --feat: display components

--- pages/Components/index.jsx
+++ pages/Components/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Header, MintingModal } from "../../components";
 
 function Components(props) {
+  const [isDisplayingModal, setIsDisplayingModal] = useState(false);
   return (
-    <div>
-      Hi Components
-    </div>
+    <>
+      <Header />
+      <main>
+        <section className="container">
+          <div className="py-4">Hi Components Page</div>
+          <button
+            className="border-2 rounded p-3 hover:bg-orange-200"
+            onClick={() => setIsDisplayingModal(true)}
+          >
+            Click to open modal
+          </button>
+          <MintingModal
+            isActive={isDisplayingModal}
+            setIsActive={setIsDisplayingModal}
+          />
+        </section>
+      </main>
+    </>
   );
 }
 
