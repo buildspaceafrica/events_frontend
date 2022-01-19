e5c5cef5aedad43f9a52465081b585598a0fe77e
davien21 <chidiebereekennia@gmail.com>
2022-01-19 08:13:48 +0100

    --feat: add toastify for notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/index.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "tailwindcss/tailwind.css";
+
+import "../styles/index.css";
+import { AppProvider } from "../contexts/appContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
