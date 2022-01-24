936b1389fbb14c2ac738ea54e485b2a9b39261da
davien21 <chidiebereekennia@gmail.com>
2022-01-24 05:46:11 +0100

    implement countdown

--- pages/event/index.jsx
+++ pages/event/index.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import react, { useState, useEffect } from "react";
 import { Add, Calendar, Clock } from "../../assets/images/svgs";
 import { Header, MintingModal } from "../../components";
 import Button from "../../components/Button";
@@ -7,10 +7,33 @@ import Video from "../../components/video";
 import styles from "./event.module.scss";
 
 function Event() {
+  const eventTime = new Date(2022, 0, 29);
+  const getTimeLeft = () => {
+    const now = new Date().getTime();
+    let difference = Math.abs(eventTime - now) / 1000;
+
+    const days = Math.floor(difference / 86400);
+    difference -= days * 86400;
+
+    const hours = Math.floor(difference / 3600) % 24;
+    difference -= hours * 3600;
+
+    const minutes = Math.floor(difference / 60) % 60;
+    difference -= minutes * 60;
+
+    const seconds = Math.round(difference % 60);
+    return { days, hours, minutes, seconds };
+  };
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+  useEffect(() => {
+    setInterval(() => {
+      setTimeLeft(getTimeLeft());
+    }, 1000);
+  });
   const [isDisplayingModal, setIsDisplayingModal] = useState(false);
 
   return (
-    <main className={styles.event__con}>
+    <div className={styles.event__con}>
       <MintingModal
         isActive={isDisplayingModal}
         setIsActive={setIsDisplayingModal}
@@ -23,7 +46,7 @@ function Event() {
             <h1>
               Buildspace <span>Africa</span> Conference 2022 is coming!
             </h1>
-            <h6>
+            <h6 className="py-5">
               Secure your position for a spot at the physical event by minting
               an NFT ticket that would be required to attend the event.
             </h6>
@@ -34,15 +57,17 @@ function Event() {
               onClick={() => setIsDisplayingModal(true)}
             />
           </div>
-          <div className={styles.right__con}>
-            <Video />
-            <div className={styles.clock__con}>
-              <Time type="day" number="05" />
-              <Time type="hour" number="04" />
-              <Time type="minute" number="23" />
-              <Time number="52" />
+          <div className={`${styles.right__con} flex flex-col py-10`}>
+            <div className="order-3 md:order-1">
+              <Video />
             </div>
-            <div className={styles.clock__bottom}>
+            <div className={`order-2 flex gap-x-8 pt-10 pb-4 lg:pb-10`}>
+              <Time type="day" number={timeLeft.days} />
+              <Time type="hour" number={timeLeft.hours} />
+              <Time type="minute" number={timeLeft.minutes} />
+              <Time number={timeLeft.seconds} />
+            </div>
+            <div className={`order-1 md:order-3 ${styles.clock__bottom}`}>
               <div className={styles.clock__bottom__left}>
                 <span>
                   <Calendar className="mr-2" />
@@ -61,7 +86,7 @@ function Event() {
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
