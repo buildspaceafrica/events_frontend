a5b6eb7ae454f46ec5ff2b31788692160f3c0aca
Joshua Nwankwo <u.joshuanwankwo@gmail.com>
2022-01-27 08:19:13 +0100

    error: please don't merge

--- components/AddToCalender/index.jsx
+++ components/AddToCalender/index.jsx
@@ -1,101 +1,102 @@
 import React, { useEffect, useState } from "react";
 import { Add } from "../../assets/images/svgs";
 import styles from "./addtocalender.module.scss";
-import AddToCalendar from "react-add-to-calendar";
-import AddToCalendarHOC from "react-add-to-calendar-hoc";
+import gapi from "googleapis";
+// import AddToCalendar from "react-add-to-calendar";
+// import AddToCalendarHOC from "react-add-to-calendar-hoc";
 
 function AddToCalender() {
-  //   const [currentWindow, setCurrentWindow] = useState(null);
-  //   useEffect(() => {
-  //     if (window) {
-  //       setCurrentWindow(window.gapi);
-  //       console.log(window);
-  //     } else {
-  //       console.log("null");
-  //     }
-  //   }, [currentWindow?.gapi]);
+  const [currentWindow, setCurrentWindow] = useState(null);
+  useEffect(() => {
+    if (window) {
+      setCurrentWindow(window);
+      console.log(window);
+    } else {
+      console.log("null");
+    }
+  }, [currentWindow?.gapi]);
 
-  //   const CLIENT_ID =
-  //     "102770639027-tvaqt3679ooq80eau0jo3moild1uffl2.apps.googleusercontent.com";
-  //   const API_KEY = "AIzaSyD_y-hErmV-vpaRgFhJUC-VCZhVh0TnVTs";
-  //   const DISCOVERY_DOCS = [
-  //     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  //   ];
-  //   const SCOPES = "https://www.googleapis.com/auth/calendar.events";
+  const CLIENT_ID =
+    "102770639027-tvaqt3679ooq80eau0jo3moild1uffl2.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyD_y-hErmV-vpaRgFhJUC-VCZhVh0TnVTs";
+  const DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+  const SCOPES = "https://www.googleapis.com/auth/calendar.events";
 
-  //   const handleClick = () => {
-  //     currentWindow?.load("client:auth2", () => {
-  //       console.log("loaded client");
+  const handleClick = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
 
-  //       currentWindow.client.init({
-  //         apiKey: API_KEY,
-  //         clientId: CLIENT_ID,
-  //         discoveryDocs: DISCOVERY_DOCS,
-  //         scope: SCOPES,
-  //       });
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
 
-  //       currentWindow?.client.load("calendar", "v3", () => console.log("bam!"));
+      gapi.client.load("calendar", "v3", () => console.log("bam!"));
 
-  //       currentWindow?.auth2
-  //         .getAuthInstance()
-  //         .signIn()
-  //         .then(() => {
-  //           var event = {
-  //             summary: "Awesome Event!",
-  //             location: "800 Howard St., San Francisco, CA 94103",
-  //             description: "Really great refreshments",
-  //             start: {
-  //               dateTime: "2020-06-28T09:00:00-07:00",
-  //               timeZone: "America/Los_Angeles",
-  //             },
-  //             end: {
-  //               dateTime: "2020-06-28T17:00:00-07:00",
-  //               timeZone: "America/Los_Angeles",
-  //             },
-  //             recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
-  //             attendees: [
-  //               { email: "lpage@example.com" },
-  //               { email: "sbrin@example.com" },
-  //             ],
-  //             reminders: {
-  //               useDefault: false,
-  //               overrides: [
-  //                 { method: "email", minutes: 24 * 60 },
-  //                 { method: "popup", minutes: 10 },
-  //               ],
-  //             },
-  //           };
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() => {
+          var event = {
+            summary: "Awesome Event!",
+            location: "800 Howard St., San Francisco, CA 94103",
+            description: "Really great refreshments",
+            start: {
+              dateTime: "2020-06-28T09:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            end: {
+              dateTime: "2020-06-28T17:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
+            attendees: [
+              { email: "lpage@example.com" },
+              { email: "sbrin@example.com" },
+            ],
+            reminders: {
+              useDefault: false,
+              overrides: [
+                { method: "email", minutes: 24 * 60 },
+                { method: "popup", minutes: 10 },
+              ],
+            },
+          };
 
-  //           var request = currentWindow.client.calendar.events.insert({
-  //             calendarId: "primary",
-  //             resource: event,
-  //           });
+          var request = gapi.client.calendar.events.insert({
+            calendarId: "primary",
+            resource: event,
+          });
 
-  //           request.execute((event) => {
-  //             console.log(event);
-  //             window.open(event.htmlLink);
-  //           });
+          request.execute((event) => {
+            console.log(event);
+            window.open(event.htmlLink);
+          });
 
-  //           /*
-  //           Uncomment the following block to get events
-  //       */
-  //           /*
-  //       // get events
-  //       currentWindow.client.calendar.events.list({
-  //         'calendarId': 'primary',
-  //         'timeMin': (new Date()).toISOString(),
-  //         'showDeleted': false,
-  //         'singleEvents': true,
-  //         'maxResults': 10,
-  //         'orderBy': 'startTime'
-  //       }).then(response => {
-  //         const events = response.result.items
-  //         console.log('EVENTS: ', events)
-  //       })
-  //       */
-  //         });
-  //     });
-  //   };
+          /*
+            Uncomment the following block to get events
+        */
+          /*
+        // get events
+        currentWindow.client.calendar.events.list({
+          'calendarId': 'primary',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        }).then(response => {
+          const events = response.result.items
+          console.log('EVENTS: ', events)
+        })
+        */
+        });
+    });
+  };
 
   //   let event = {
   //     title: "Sample Event",
@@ -118,25 +119,24 @@ function AddToCalender() {
   //     title: "Super Fun Event",
   //   };
 
-  const AddToCalendarModal = AddToCalendarHOC(Button, CalendarModal);
+  //   const AddToCalendarModal = AddToCalendarHOC(Button, CalendarModal);
 
   return (
     <div
       className={styles.clock__bottom__right}
-      //   onClick={() => {
-      //     handleClick();
-      //   }}
+      onClick={() => {
+        handleClick();
+      }}
     >
-      {/* <Add className="mr-2" />
-      <h1>Add to Calender</h1> */}
-      {/* <AddToCalendar event={event} buttonLabel="Put on my calendar" /> */}
-      <AddToCalendarModal
+      <Add className="mr-2" />
+      <h1>Add to Calender</h1>
+      {/* <AddToCalendarModal
         className={componentStyles}
         linkProps={{
           className: linkStyles,
         }}
         event={event}
-      />
+      /> */}
     </div>
   );
 }
--- components/Video/index.jsx
+++ components/Video/index.jsx
@@ -6,26 +6,7 @@ import styles from "./video.module.scss";
 function Video() {
   return (
     <div className={styles.video__con}>
-      {/* <Image
-        objectFit="cover"
-        src={
-          "https://res.cloudinary.com/davien21/image/upload/v1643058179/video_gzz0xp.png"
-        }
-        height={536}
-        width={448}
-        quality={100}
-        alt=""
-        placeholder="blur"
-        blurDataURL="https://res.cloudinary.com/davien21/image/upload/c_scale,e_blur:100,q_54,w_379/v1643058179/video_gzz0xp.png"
-      /> */}
-      {/* <iframe
-        width="548"
-        height="536"
-        src="https://www.youtube.com/embed/tgbNymZ7vqY"
-      ></iframe> */}
       <iframe
-        // width="790"
-        // height="444"
         src="https://www.youtube.com/embed/my-DR6xY_TA"
         title="YouTube video player"
         frameBorder="0"
@@ -35,7 +16,7 @@ function Video() {
 
       <span className={styles.video__action}>
         <PlayButton />
-        <h2>Play Promotional Video</h2>
+        <h2>Promotional Video</h2>
       </span>
     </div>
   );
