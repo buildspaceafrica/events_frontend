e61953984aa19f57ed869da2f63c6617cc710e7f
davien21 <chidiebereekennia@gmail.com>
2022-01-27 08:30:35 +0100

    --feat: minor fixes and add confetti

--- components/MintingModal/Congrats/index.jsx
+++ components/MintingModal/Congrats/index.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import styles from "./congrats.module.scss";
 import { Button } from "../..";
 
-function Congrats() {
+function Congrats({ onClose }) {
   return (
     <div className={styles.congrats__con}>
       <h1>
-        <span>Congratulations! </span> 
+        <span>Congratulations! </span>
       </h1>
       <h5>
         Your NFT is ready! Open your email to view and download the NFT ticket.
         See you at the event 🚀
       </h5>
       <span className={styles.congrats__button__con}>
-        <Button text="Done" type="primary" />
+        <Button onClick={onClose} text="Done" type="primary" />
       </span>
     </div>
   );
