5e4623276ebe64615929c5779b6c6bbf2464d65a
Somtozech <32250849+Somtozech@users.noreply.github.com>
2025-07-15 14:54:38 +0100

    Updated next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  swcMinify: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/index",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
